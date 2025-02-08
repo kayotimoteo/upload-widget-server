868207c9c8bf071993130a20dc1de10ad9c89468
Kayo Timoteo <timoteokayo@gmail.com>
2025-02-08 15:48:44 -0400

    initial commit

---
+++ src/app/functions/export-uploads.spec.ts
@@ -0,0 +1,80 @@
+import { randomUUID } from 'node:crypto'
+import * as upload from '@/infra/storage/upload-file-to-storage'
+import { isRight, unwrapEither } from '@/shared/either'
+import { makeUpload } from '@/test/factories/make-upload'
+import { beforeAll, describe, expect, it, vi } from 'vitest'
+import { exportUploads } from './export-uploads'
+
+describe('export uploads', () => {
+  beforeAll(() => {
+    vi.mock('@/infra/storage/upload-file-to-storage', () => {
+      return {
+        uploadFileToStorage: vi.fn().mockImplementation(() => {
+          return {
+            key: `${randomUUID()}.jpg`,
+            url: 'https://storage.com/image.jpg',
+          }
+        }),
+      }
+    })
+  })
+
+  it('should be able to export uploads', async () => {
+    const uploadStub = vi
+      .spyOn(upload, 'uploadFileToStorage')
+      .mockImplementationOnce(async () => {
+        return {
+          key: `${randomUUID()}.csv`,
+          url: 'http://example.com/file.csv',
+        }
+      })
+
+    const namePattern = randomUUID()
+
+    const upload1 = await makeUpload({ name: `${namePattern}.webp` })
+    const upload2 = await makeUpload({ name: `${namePattern}.webp` })
+    const upload3 = await makeUpload({ name: `${namePattern}.webp` })
+    const upload4 = await makeUpload({ name: `${namePattern}.webp` })
+    const upload5 = await makeUpload({ name: `${namePattern}.webp` })
+
+    const sut = await exportUploads({
+      searchQuery: namePattern,
+    })
+
+    const generatedSCVSteam = uploadStub.mock.calls[0][0].contentStream
+
+    const csvAsString = await new Promise<string>((resolve, reject) => {
+      const chunks: Buffer[] = []
+
+      generatedSCVSteam.on('data', chunk => {
+        chunks.push(chunk)
+      })
+
+      generatedSCVSteam.on('end', () => {
+        resolve(Buffer.concat(chunks).toString('utf-8'))
+      })
+
+      generatedSCVSteam.on('error', err => {
+        reject(err)
+      })
+    })
+
+    const csvAsArray = csvAsString
+      .trim()
+      .split('\n')
+      .map(row => row.split(','))
+
+    expect(isRight(sut)).toBe(true)
+    expect(unwrapEither(sut)).toEqual({
+      reportUrl: 'http://example.com/file.csv',
+    })
+    expect(csvAsArray).toEqual([
+      ['ID', 'Name', 'URL', 'Uploaded at'],
+      [upload1.id, upload1.name, upload1.remoteUrl, expect.any(String)],
+      [upload2.id, upload2.name, upload2.remoteUrl, expect.any(String)],
+      [upload3.id, upload3.name, upload3.remoteUrl, expect.any(String)],
+      [upload4.id, upload4.name, upload4.remoteUrl, expect.any(String)],
+      [upload5.id, upload5.name, upload5.remoteUrl, expect.any(String)],
+    ])
+  })
+})
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,11 +1,60 @@
 import { fastifyCors } from '@fastify/cors'
+import fastifyMultipart from '@fastify/multipart'
+import fastifySwagger from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { exportUploadsRoute } from './routes/exports-uploads'
+import { getUploadsRoute } from './routes/get-uploads'
+import { uploadImageRoute } from './routes/upload-image'
+import { transformSwaggerSchema } from './transform-swagger-schema'
 
 const server = fastify()
+
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  // Enviar erro para OTel
+
+  console.error(error)
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
 
 server.register(fastifyCors, {
   origin: '*',
 })
+
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Upload Server',
+      version: '1.0.0',
+    },
+  },
+  transform: transformSwaggerSchema,
+})
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+server.register(uploadImageRoute)
+server.register(getUploadsRoute)
+server.register(exportUploadsRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
